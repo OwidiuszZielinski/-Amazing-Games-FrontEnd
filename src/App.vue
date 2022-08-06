0df0661ea1228116f7ab83a37530bf46e5e74b5c
root <root@DESKTOP-E9R1BQM.localdomain>
2022-08-07 00:32:46 +0200

    startproject

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,104 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <div id="app">
+  <v-app id="inspire">
+    <v-app id="inspire">
+      <v-navigation-drawer
+        v-model="drawer"
+        app
+        clipped
+      >
+        <v-list dense>
+          <v-list-item link v-on:click = "route('/about')">
+            <v-list-item-action>
+              <v-icon>mdi-play</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Games</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-action>
+              <v-icon>mdi-currency-usd</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Orders</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+           <v-list-item link >
+            <v-list-item-action>
+              <v-icon>mdi-account-multiple</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Users</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+  
+      <v-app-bar
+        app
+        clipped-left
+      >
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>Amazing Games
+        </v-toolbar-title>
+      </v-app-bar>
+  
+      <v-main>
+        <v-container
+          class="fill-height"
+          
+        >
+         <router-view/>
+
+        </v-container>
+      </v-main>
+  
+      <v-footer app>
+        <span>&copy; 2022 Owidiusz Zieliński</span>
+      </v-footer>
+    </v-app>
+  </v-app>
+</div>
+
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
+<script>
+import router from './router'
+import { Icon } from '@iconify/vue2';
+export default{
+  methods : {
+    route(link){
+      router.push(link)
+    }
+  },
+  componets:{
+    Icon
+  }
+}
+
+
+</script>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>{{ info.data.value}}</h1>
+  </div>
+</template>
+<script>
+// eslint-disable-next-line no-unused-vars
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://www.tronalddump.io/random/quote')
+      .then(response => (this.info = response))
+  }
+}
+</script>
