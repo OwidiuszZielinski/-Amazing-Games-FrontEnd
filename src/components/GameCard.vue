421f82f6c16623d6fdedb277354dc7c842c4b534
Owidiusz Zieliński <owi19955@gmail.com>
2022-08-13 18:17:46 +0200

    add game add card

--- src/components/GameCard.vue
+++ src/components/GameCard.vue
@@ -1,15 +1,12 @@
 <template>
 
+
   <v-card 
     
     class="d-flex align-content-start flex-wrap mx-1 mt-1 "
     max-width="344"
   >
-<<<<<<< HEAD
-  <MoreInfoDialog v-model="showScheduleForm"></MoreInfoDialog>
-=======
   
->>>>>>> infodialog
     <v-card-text style="height: 100%; background: url('CardBackground.jpg') no-repeat center center" justify="center" >
       
       <p class="text-h4 text--primary" >
@@ -28,18 +25,7 @@
         <v-col  md = "5" align-center >
       
     <v-card-actions >
-<<<<<<< HEAD
-       <v-btn 
-        text
-        color="deep-purple accent-4"
-        @click.stop="showScheduleForm = true"
-      >
-      
-        MORE INFO
-      </v-btn>
-=======
       <MoreInfoDialog v-model="showScheduleForm"></MoreInfoDialog>
->>>>>>> infodialog
        <v-btn
        
          text
@@ -47,12 +33,15 @@
       >
       <v-icon>mdi-cart</v-icon>
         BUY
+
       </v-btn>
         
     </v-card-actions>
+
         </v-col>
     </v-row>
     </v-card-actions>
+
   </v-card>
   
 </template>
@@ -61,18 +50,6 @@
 import MoreInfoDialog from './MoreInfoDialog.vue';
 
   export default {
-<<<<<<< HEAD
-    data() {
-        return {
-            games :[],
-            showScheduleForm: false,
-            
-        };
-    },
-   
-    props: ["groupe", "title", "price", "rating","description"],
-    components: { MoreInfoDialog }
-=======
     
     data() {
         return {
@@ -83,6 +60,5 @@ import MoreInfoDialog from './MoreInfoDialog.vue';
    
     props: ["gameGroup", "title", "price", "rating","description"],
     components: { MoreInfoDialog },
->>>>>>> infodialog
 }
 </script>
--- src/components/MoreInfoDialog.vue
+++ src/components/MoreInfoDialog.vue
@@ -1,60 +1,6 @@
 <template>
   <v-row justify="space-around">
     <v-col cols="auto">
-<<<<<<< HEAD
-      <v-dialog
-        transition="dialog-bottom-transition"
-        max-width="600"
-        v-model="show"
-      >
-        <template v-slot:default="dialog">
-          <v-card>
-            <v-toolbar
-              color="primary"
-              dark
-            >Opening from the bottom</v-toolbar>
-            <v-card-text>
-              <div class="text-h2 pa-12">{{description}}</div>
-            </v-card-text>
-            <v-card-actions class="justify-end">
-              <v-btn
-                text
-                @click="dialog.value = false"
-              >Close</v-btn>
-            </v-card-actions>
-          </v-card>
-        </template>
-      </v-dialog>
-    </v-col>
-
-    <v-col cols="auto">
-      <v-dialog
-        transition="dialog-top-transition"
-        max-width="600"
-      >
-      </v-dialog>
-    </v-col>
-  </v-row>
-</template>
-<script>
-export default {
-  props: { 
-     value: Boolean,
-     description: String
-  },
- 
-  computed: {
-    show: {
-      get () {
-        return this.value
-      },
-      set (value) {
-         this.$emit('input', value)
-      }
-    }
-  }
-  
-=======
      <v-spacer></v-spacer>
         <v-dialog
           v-model="dialog"
@@ -114,6 +60,5 @@ export default {
         this.dialog = false
       },
     }
->>>>>>> infodialog
 }
 </script>
